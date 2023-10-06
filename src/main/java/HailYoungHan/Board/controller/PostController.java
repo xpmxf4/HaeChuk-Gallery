a6689ddca66e2eb38d3b6eadb5528070ae7eac55
dev-chpark <xpmxf5@gmail.com>
2023-10-06 14:33:32 +0900

    ⚙️ chore: Post api 기본 경로 수정 /api/posts -> /posts

--- src/main/java/HailYoungHan/Board/controller/PostController.java
+++ src/main/java/HailYoungHan/Board/controller/PostController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/posts")
+@RequestMapping("/posts")
 public class PostController {
 
     private final PostService postService;
