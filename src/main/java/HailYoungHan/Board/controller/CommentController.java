62264a9960c97e2ee9b598c7fc2d73d3e3d50ea1
dev-chpark <xpmxf5@gmail.com>
2023-10-09 19:25:53 +0900

    ⭐ feat: CommentController addComment 추가

---
+++ src/main/java/HailYoungHan/Board/controller/CommentController.java
@@ -0,0 +1,24 @@
+package HailYoungHan.Board.controller;
+
+import HailYoungHan.Board.dto.CommentRegiDTO;
+import HailYoungHan.Board.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller("/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> addComment(@RequestBody CommentRegiDTO dto) {
+
+        commentService.addComment(dto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
