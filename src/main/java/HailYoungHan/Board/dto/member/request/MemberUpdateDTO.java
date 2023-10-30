ff5ad8ecd3718c01fe3710a6f50dbd2e9b76bf13
dev-chpark <xpmxf5@gmail.com>
2023-10-30 15:44:00 +0900

    ♻️ refactor: MemberUpdateDTO 의 모든 필드 @NotNull -> @NotBlank

--- src/main/java/HailYoungHan/Board/dto/member/request/MemberUpdateDTO.java
+++ src/main/java/HailYoungHan/Board/dto/member/request/MemberUpdateDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -13,14 +14,14 @@ import javax.validation.constraints.Size;
 //@AtLeastOneNotNull(fields = {"name", "email", "password"})
 public class MemberUpdateDTO {
     @Size(min = 2, max = 50, message = "이름은 2자 이상, 50자 이하입니다.")
-    @Nullable // @NotBlank 박으라고
+    @NotBlank
     private String name;
 
     @Pattern(regexp = "^[A-Za-z]+@[A-Za-z]+\\.[A-Za-z]{2,}$")
-    @Nullable // @NotBlank
+    @NotBlank
     private String email;
 
     @Size(min = 8, message = "비밀번호는 최소 8자리 이상입니다.")
-    @Nullable // @NotBlank
+    @NotBlank
     private String password;
 }
