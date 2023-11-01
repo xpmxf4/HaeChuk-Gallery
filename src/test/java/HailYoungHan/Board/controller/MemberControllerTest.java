2b31088f2903e625f1d013397218573c9e4bc4b3
dev-chpark <xpmxf5@gmail.com>
2023-11-01 10:34:21 +0900

    🚦 test: MemberController 의 getAllMembers() 정상 작동 케이스 작성

--- src/test/java/HailYoungHan/Board/controller/MemberControllerTest.java
+++ src/test/java/HailYoungHan/Board/controller/MemberControllerTest.java
@@ -2,6 +2,7 @@ package HailYoungHan.Board.controller;
 
 import HailYoungHan.Board.dto.member.query.MemberDbDTO;
 import HailYoungHan.Board.dto.member.request.MemberRegiDTO;
+import HailYoungHan.Board.dto.member.response.MemberResponseDTO;
 import HailYoungHan.Board.service.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -63,7 +67,8 @@ class MemberControllerTest {
                 .willReturn(expectedMember);
 
         // when - 동작
-        ResultActions perform = mockMvc.perform(get("/members/" + memberId).contentType(MediaType.APPLICATION_JSON));
+        ResultActions perform = mockMvc.perform(get("/members/" + memberId)
+                .contentType(MediaType.APPLICATION_JSON));
 
         // then - 검증
         perform
@@ -74,7 +79,34 @@ class MemberControllerTest {
     }
 
     @Test
-    void testGetAllMembers() {
+    public void testGetAllMembers() throws Exception {
+        // given - 상황 만들기
+        List<MemberDbDTO> expectedMembers = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            MemberDbDTO member = MemberDbDTO.builder()
+                    .id((long) i)
+                    .name("test" + i)
+                    .email("test" + i + "@gmail.com")
+                    .build();
+
+            expectedMembers.add(member);
+        }
+        MemberResponseDTO expectedResult = new MemberResponseDTO(expectedMembers);
+
+        given(memberService.getAllMembers())
+                .willReturn(expectedResult);
+
+        //when - 동작
+        ResultActions perform = mockMvc.perform(get("/members")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then - 검증
+        for (int i = 0; i < 5; i++) {
+            perform
+                    .andExpect(jsonPath("$.members[" + i + "].id", is(expectedMembers.get(i).getId().intValue())))
+                    .andExpect(jsonPath("$.members[" + i + "].name", is(expectedMembers.get(i).getName())))
+                    .andExpect(jsonPath("$.members[" + i + "].email", is(expectedMembers.get(i).getEmail())));
+        }
     }
 
     @Test
