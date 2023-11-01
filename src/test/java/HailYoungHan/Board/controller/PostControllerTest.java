82bd72544c6201d5e260a8221d38f0db358d1dcd
dev-chpark <xpmxf5@gmail.com>
2023-11-01 11:53:00 +0900

    🚦 test: PostController register 정상 케이스 테스트

--- src/test/java/HailYoungHan/Board/controller/PostControllerTest.java
+++ src/test/java/HailYoungHan/Board/controller/PostControllerTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,7 +23,7 @@ class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
+    @MockBean
     private PostService postService;
 
     @Test
