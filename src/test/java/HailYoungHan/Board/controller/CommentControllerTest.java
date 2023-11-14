23ba9d1533aa50affc3d05971429c139d5d7081d
dev-chpark <xpmxf5@gmail.com>
2023-11-14 10:23:07 +0900

    🚦 test: CommentControllerTest 주석 추가, 메서드 이름 수정, 예외 케이스 추가

--- src/test/java/HailYoungHan/Board/controller/CommentControllerTest.java
+++ src/test/java/HailYoungHan/Board/controller/CommentControllerTest.java
@@ -1,12 +1,13 @@
 package HailYoungHan.Board.controller;
 
-import HailYoungHan.Board.controller.CommentController;
 import HailYoungHan.Board.dto.comment.query.CommentDbDTO;
 import HailYoungHan.Board.dto.comment.request.CommentRegiDTO;
 import HailYoungHan.Board.dto.comment.request.CommentUpdateDTO;
 import HailYoungHan.Board.dto.comment.response.CommentResponseDTO;
+import HailYoungHan.Board.exception.CustomException;
 import HailYoungHan.Board.service.CommentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,13 +19,17 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.Arrays;
 import java.util.List;
 
+import static HailYoungHan.Board.exception.ErrorCode.*;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CommentController.class)
 class CommentControllerTest {
@@ -40,7 +45,8 @@ class CommentControllerTest {
 
     // 댓글 생성 테스트
     @Test
-    public void test_Create_Comment_Success() throws Exception {
+    @DisplayName("댓글 생성 - 유효한 데이터로 성공")
+    void createComment_ShouldReturnCreated_WhenDataIsValid() throws Exception {
         // given - 상황 설정
         CommentRegiDTO commentRegiDTO = CommentRegiDTO.builder()
                 .content("content")
@@ -61,7 +67,8 @@ class CommentControllerTest {
 
     // 댓글 수정 테스트
     @Test
-    public void test_Update_Comment_Success() throws Exception {
+    @DisplayName("댓글 수정 - 유효한 데이터로 성공")
+    void updateComment_ShouldReturnOk_WhenDataIsValid() throws Exception {
         // given - 상황 설정
         long commentId = 1L;
         CommentUpdateDTO updateDTO = CommentUpdateDTO.builder()
@@ -82,7 +89,8 @@ class CommentControllerTest {
 
     // ID로 단일 댓글 조회 테스트
     @Test
-    public void test_Get_Single_Comment_By_Id_Success() throws Exception {
+    @DisplayName("단일 댓글 조회 - 존재하는 댓글 ID로 성공")
+    void getSingleCommentById_ShouldReturnComment_WhenCommentExists() throws Exception {
         // given - 상황 설정
         Long commentId = 1L;
         CommentDbDTO expectedComment = CommentDbDTO.builder()
@@ -106,7 +114,8 @@ class CommentControllerTest {
 
     // 모든 댓글 조회 테스트
     @Test
-    public void test_Get_All_Comments_Success() throws Exception {
+    @DisplayName("모든 댓글 조회 - 성공")
+    void getAllComments_ShouldReturnAllComments_WhenCalled() throws Exception {
         // given - 상황 설정
         List<CommentDbDTO> allComments = Arrays.asList(
                 new CommentDbDTO("content1", false),
@@ -130,7 +139,8 @@ class CommentControllerTest {
 
     // 특정 멤버의 모든 댓글 조회 테스트
     @Test
-    public void test_Get_Comments_By_MemberId_Success() throws Exception {
+    @DisplayName("특정 멤버의 모든 댓글 조회 - 성공")
+    void getCommentsByMemberId_ShouldReturnComments_WhenMemberExists() throws Exception {
         // given - 상황 설정
         Long memberId = 1L;
         List<CommentDbDTO> memberComments = Arrays.asList(
@@ -156,7 +166,8 @@ class CommentControllerTest {
 
     // 특정 멤버의 삭제된 댓글 조회 테스트
     @Test
-    public void test_Get_Deleted_Comments_By_MemberId_Success() throws Exception {
+    @DisplayName("특정 멤버의 삭제된 댓글 조회 - 성공")
+    void getDeletedCommentsByMemberId_ShouldReturnDeletedComments_WhenMemberExists() throws Exception {
         // given - 상황 설정
         Long memberId = 1L;
         List<CommentDbDTO> deletedComments = Arrays.asList(
@@ -183,7 +194,8 @@ class CommentControllerTest {
 
     // 댓글 삭제 테스트
     @Test
-    public void test_Delete_Comment_Success() throws Exception {
+    @DisplayName("댓글 삭제 - 성공")
+    void deleteComment_ShouldReturnOk_WhenCommentExists() throws Exception {
         // given - 상황 설정
         long commentId = 1L;
 
@@ -195,5 +207,92 @@ class CommentControllerTest {
 
         // then - 검증
         perform.andExpect(status().isOk());
+    }
+
+    /* 예외 케이스 */
+    // 댓글 생성 실패 테스트 - 부적절한 데이터
+    @Test
+    @DisplayName("댓글 생성 - 부적절한 데이터로 실패")
+    void createComment_ShouldReturnBadRequest_WhenDataIsInvalid() throws Exception {
+        CommentRegiDTO invalidDto = CommentRegiDTO.builder()
+                .content("")
+                .build();
+
+        mockMvc.perform(post("/comments")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    // 댓글 수정 실패 테스트 - 존재하지 않는 댓글 ID
+    @Test
+    @DisplayName("댓글 수정 - 존재하지 않는 댓글 ID로 실패")
+    void updateComment_ShouldReturnNotFound_WhenCommentDoesNotExist() throws Exception {
+        long nonExistingId = Long.MAX_VALUE;
+        CommentUpdateDTO updateDTO = CommentUpdateDTO.builder()
+                .content("Updated content")
+                .build();
+
+        doThrow(new CustomException(COMMENT_NOT_FOUND_BY_ID, nonExistingId))
+                .when(commentService)
+                .updateComment(eq(nonExistingId), any(CommentUpdateDTO.class));
+
+        mockMvc.perform(put("/comments/" + nonExistingId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateDTO)))
+                .andExpect(status().isNotFound());
+    }
+
+    // 단일 댓글 조회 실패 테스트 - 존재하지 않는 댓글 ID
+    @Test
+    @DisplayName("단일 댓글 조회 - 존재하지 않는 댓글 ID로 실패")
+    void getSingleCommentById_ShouldReturnNotFound_WhenCommentDoesNotExist() throws Exception {
+        Long nonExistingId = Long.MAX_VALUE;
+        given(commentService.getSingleCommentById(nonExistingId))
+                .willThrow(new CustomException(COMMENT_NOT_FOUND_BY_ID, nonExistingId));
+
+        mockMvc.perform(get("/comments/" + nonExistingId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    // 특정 멤버의 모든 댓글 조회 실패 테스트 - 존재하지 않는 멤버 ID
+    @Test
+    @DisplayName("특정 멤버의 모든 댓글 조회 - 존재하지 않는 멤버 ID로 실패")
+    void getCommentsByMemberId_ShouldReturnNotFound_WhenMemberDoesNotExist() throws Exception {
+        Long nonExistingMemberId = Long.MAX_VALUE;
+        given(commentService.getMemberAllComments(nonExistingMemberId))
+                .willThrow(new CustomException(MEMBER_NOT_FOUND_BY_ID, nonExistingMemberId));
+
+        mockMvc.perform(get("/comments/members/" + nonExistingMemberId + "/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    // 특정 멤버의 삭제된 댓글 조회 실패 테스트 - 존재하지 않는 멤버 ID
+    @Test
+    @DisplayName("특정 멤버의 삭제된 댓글 조회 - 존재하지 않는 멤버 ID로 실패")
+    void getDeletedCommentsByMemberId_ShouldReturnNotFound_WhenMemberDoesNotExist() throws Exception {
+        Long nonExistingMemberId = Long.MAX_VALUE;
+        given(commentService.getMemberAllDeletedComments(nonExistingMemberId))
+                .willThrow(new CustomException(MEMBER_NOT_FOUND_BY_ID, nonExistingMemberId));
+
+        mockMvc.perform(get("/comments/members/" + nonExistingMemberId + "/isDeleted")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    // 댓글 삭제 실패 테스트 - 존재하지 않는 댓글 ID
+    @Test
+    @DisplayName("댓글 삭제 - 존재하지 않는 댓글 ID로 실패")
+    void deleteComment_ShouldReturnNotFound_WhenCommentDoesNotExist() throws Exception {
+        Long nonExistingId = Long.MAX_VALUE;
+        doThrow(new CustomException(COMMENT_NOT_FOUND_BY_ID, nonExistingId))
+                .when(commentService)
+                .deleteComment(nonExistingId);
+
+        mockMvc.perform(delete("/comments/" + nonExistingId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
