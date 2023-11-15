75bd58213e6612bae40370b2bed8fe0444858139
dev-chpark <xpmxf5@gmail.com>
2023-11-15 20:16:36 +0900

    ⚙️ chore: JpaAuditingConfiguration 에 용도를 알리는 주석 추가

--- src/main/java/HailYoungHan/Board/config/JpaAuditingConfiguration.java
+++ src/main/java/HailYoungHan/Board/config/JpaAuditingConfiguration.java
@@ -3,6 +3,7 @@ package HailYoungHan.Board.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// BoardApplication 에서 @EnableJpaAuditing 제외하기 위한 설정 클래스
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfiguration {
