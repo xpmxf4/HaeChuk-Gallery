edad17d1275a0d4f98c525dc7d5cc11ba3f42f3d
dev-chpark <xpmxf5@gmail.com>
2024-04-04 14:50:35 +0900

    ⚙️ chore: 불필요한 logger import문 삭제

--- src/main/java/hailyounghan/BoardApplication.java
+++ src/main/java/hailyounghan/BoardApplication.java
@@ -1,11 +1,9 @@
 package hailyounghan;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Slf4j
 @EnableScheduling
 //@EnableJpaAuditing
 @SpringBootApplication
